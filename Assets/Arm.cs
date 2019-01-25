9852de785761c3e33aa71775f39b62acdb1dac17
Cat Flynn <me@monodokimes.uk>
2019-01-25 20:59:16 +0000

    draw super basic arm

---
+++ Assets/Arm.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    public GameObject shoulder;
+    public GameObject hand;
+
+    private LineRenderer _line;
+
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.positionCount = 2;
+    }
+
+    void Update()
+    {
+        _line.SetPositions(new[]
+        {
+            shoulder.transform.position,
+            hand.transform.position
+        });
+    }
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public float armLength;
+    [Range(0, 1)]
+    public float moveSpeed;
+
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos;
+
+        var mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var originPos = (Vector2)transform.parent.position;
+        var toMouse = mousePos - originPos;
+        var dir = toMouse.normalized;
+
+        if (toMouse.magnitude < armLength)
+        {
+            targetPos = mousePos;
+        }
+        else
+        {
+            targetPos = originPos + dir * armLength;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+    }
+}
