c83fce8eb238f41a9d2dcab064e8cebab7c6551f
Cat Flynn <me@monodokimes.uk>
2019-01-26 04:03:57 +0000

    draw arm as bezier curve

--- Assets/Scripts/Arm.cs
+++ Assets/Scripts/Arm.cs
@@ -14,18 +14,51 @@ public class Arm : MonoBehaviour
     void Start()
     {
         _line = GetComponent<LineRenderer>();
-        _line.positionCount = 2;
 
         _handRb = hand.GetComponent<Rigidbody2D>();
     }
 
     void LateUpdate()
     {
-        _line.SetPositions(new[]
+        var points = GetBezierPoints();
+        _line.positionCount = points.Length;
+        _line.SetPositions(points);
+    }
+
+    private Vector3[] GetBezierPoints()
+    {
+        var shoulderPos = shoulder.transform.position;
+        var handPos = hand.transform.position;
+        var avg = (shoulderPos + handPos) / 2f;
+        var l = (handPos - shoulderPos).magnitude;
+
+        float armDroop = 0.7f;
+
+        var minY = avg.y - l * armDroop;
+        var min = new Vector3(avg.x, minY);
+
+        var pos1 = handPos.x < shoulderPos.x ? handPos : shoulderPos;
+        var pos2 = min;
+        var pos3 = handPos.x < shoulderPos.x ? shoulderPos : handPos;
+
+        var seg1 = pos2 - pos1;
+        var seg2 = pos3 - pos2;
+
+        int steps = 10;
+        Vector3[] results = new Vector3[steps + 1];
+        float stepSize = 1f / (steps - 1f);
+        for (int i = 0; i <= steps; i++)
         {
-            shoulder.transform.position,
-            hand.transform.position
-        });
+            float t = i * stepSize;
+
+            var startPos = Vector3.Lerp(pos1, pos2, t);
+            var endPos = Vector3.Lerp(pos2, pos3, t);
+            var point = Vector3.Lerp(startPos, endPos, t);
+
+            results[i] = point;
+        }
+
+        return results;
     }
 
     private void FixedUpdate()
