bf37f1ec72a64de910155b8be706c89cdb5f0ac0
Cat Flynn <me@monodokimes.uk>
2019-01-26 15:16:43 +0000

    get kettle out of fridge

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,14 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
 public class Interactable : MonoBehaviour
 {
+    public bool active;
     public UnityEvent OnInteract = new UnityEvent();
+
+    private Collider2D _collider;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        _rb.simulated = active;
+        _collider.enabled = active;
+    }
 
     public void Interact()
     {
+        if (!active)
+            return;
+
         OnInteract.Invoke();
+    }
+
+    public void Activate()
+    {
+        if (!active)
+        {
+            active = true;
+            _rb.simulated = active;
+            _collider.enabled = active;
+        }
     }
 }
