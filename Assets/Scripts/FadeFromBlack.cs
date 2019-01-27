8e0774f4ecbf228452a4e3edbc6ebd30e1692f4a
cyndrdev <me@cyndr.uk>
2019-01-27 12:08:28 +0000

    fixed or w/e

--- Assets/Scripts/FadeFromBlack.cs
+++ Assets/Scripts/FadeFromBlack.cs
@@ -10,18 +10,32 @@ public class FadeFromBlack : MonoBehaviour
     private float _fadeAmt;
     private Image _image;
 
+    private int _endPoint;
+    private Narrative _narrative;
+    private bool _fadingOut = false;
+
     void Start()
     {
         _image = gameObject.GetComponent<Image>();
+        _narrative = Game.Instance.Narrative;
+        _endPoint = _narrative.end;
         _fadeAmt = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_fadeTime <= 0f) return; 
+        if (_fadeTime <= 0f) return;
 
-        Color _color = new Color(0f, 0f, 0f, 1f - _fadeAmt);
+        if (_narrative.position == _endPoint)
+        {
+            _fadingOut = true;
+            _fadeAmt = 0f;
+        }
+
+        if (_fadeAmt >= 1f) return;
+
+        Color _color = new Color(0f, 0f, 0f, _fadingOut ? _fadeAmt : 1f - _fadeAmt);
         _image.color = _color;
 
         if (_fadeAmt == 1f)
