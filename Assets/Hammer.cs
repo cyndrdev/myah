6b9bcea8dcb0954530649af365ace758e127dfae
Cat Flynn <me@monodokimes.uk>
2019-01-27 06:09:21 +0000

    hide hammer behind cactus, add record player to lounge

---
+++ Assets/Hammer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void UpdateRenderLayer()
+    {
+        _sprite.sortingLayerName = GameConstants.Pickups;
+        _sprite.sortingOrder = 1;
+    }
+}
