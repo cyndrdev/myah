726f488089137e89d8d8566c4b5118f16b8d953d
cyndrdev <me@cyndr.uk>
2019-01-27 00:48:48 +0000

    fixed weird sprite issues

--- Assets/Scripts/FadeFromBlack.cs
+++ Assets/Scripts/FadeFromBlack.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeFromBlack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float fadeamt;
+    private 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
